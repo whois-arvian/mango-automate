74e77a553c9326e6b1d3aff4c745f7d38265e03d
Whois Arvian <arviannewbie473@gmail.com>
2025-01-13 03:21:48 +0700

    update

---
+++ accounts/accounts_tmp.js
@@ -0,0 +1,14 @@
+/**
+ * Accounts list file
+ * write your accounts like this
+ * export const accountLists = [
+ *     "MANGOPRIVKEY1",
+ *     "MANGOPRIVKEY2",
+ *     "MANGOPRIVKEY3",
+ * ];
+ *
+ */
+
+export const accountList = [
+    "mgoprivkey1qq5tueqvehpdwjscnp7h5qq5urhp2j7yph2ygzu86s6jl00pszhq6xgprjc"
+];
